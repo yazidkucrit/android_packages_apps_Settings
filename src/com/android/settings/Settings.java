1598c3f1d4b27d561181954cf473c4eab7eecc8a
Danesh M <daneshm90@gmail.com>
2012-03-15 23:16:29 -0400

    Apn settings fixes / code maintainence (Second attempt to fix APN Settings crash)
    Depends on : http://review.cyanogenmod.org/54909
    
    Cleanup unecessary resources and use global ones
      - This fixes the duplicate id issue currently causing apn's to be un-editable
    
    Bring the ui aspect of the preference up to speed with ics
     - Use ics highlight / layout...etc
     - Use fragments
    
    Patchset 5 : Remove theme for ApnEditor
                 Use static access for variables
    Patchset 6 : Launch apn editor internally as a fragment
    	     Replace deprecated managedQuery with CursorLoader
    
    Apn Settings : Fix panel title
    
    Show appropriate apn editor title
    
    Change-Id: I1f9dbce5dbf464db79d7c2fbedb8f833e4b46468
    
    Settings - add an AlertDialog before to delete APN to avoid fails
    
    patch 1: fix blank space
    patch 2: remove FR translation and change NegativeButton string
    patch 3: improve the NegativeButton (thank you, David)
    patch 4: remove create()
    
    Change-Id: I3361d4160a7e1c4eaee094f08c7d212f71e9e0e7
    
    * Forward port from cm-10.2, adjusted for 4.4
    
    Change-Id: Ie63fa1018ffb7bc419e4d7915419657de30f5b22
    
    Conflicts:
    
    	AndroidManifest.xml
    	src/com/android/settings/Settings.java
    
    Conflicts:
    	AndroidManifest.xml
    	res/values/cm_strings.xml
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1152,5 +1152,7 @@ public class Settings extends PreferenceActivity
     public static class PaymentSettingsActivity extends Settings { /* empty */ }
     public static class PrintSettingsActivity extends Settings { /* empty */ }
     public static class PrintJobSettingsActivity extends Settings { /* empty */ }
+    public static class ApnSettingsActivity extends Settings { /* empty */ }
+    public static class ApnEditorActivity extends Settings { /* empty */ }
     public static class ASSRamBarActivity extends Settings { /* empty */ }
 }
