cf0a038970f0907fb11b7c5e0f97d9ef3b3c3493
MURA Stefano <ssspinni@gmail.com>
2013-12-21 16:12:17 +0100

    [2/2] Clear All Apps button added in Recent Panel
    Thanks to tectas
    Mine rearranged, modified and adapted.
    
    Change-Id: If1cd49f3612676090f82098b2a21b87e36fd471b

--- src/com/android/settings/AOSPAL/SystemSettings.java
+++ src/com/android/settings/AOSPAL/SystemSettings.java
@@ -4,15 +4,18 @@ import android.app.ActivityManager;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.PreferenceCategory;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.util.Log;
 
 import com.android.internal.util.paranoid.DeviceUtils;
 
@@ -22,11 +25,16 @@ import com.android.settings.SettingsPreferenceFragment;
 public class SystemSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private static final String TAG = "SystemSettings";
     private static final String SYSTEM_SETTINGS = "system_settings";
 
     private static final String KEY_DUAL_PANEL = "force_dualpanel";
     private static final String KEY_REVERSE_DEFAULT_APP_PICKER = "reverse_default_app_picker";
+    private static final String RECENT_MENU_CLEAR_ALL = "recent_menu_clear_all";
+    private static final String RECENT_MENU_CLEAR_ALL_LOCATION = "recent_menu_clear_all_location";
 
+    private ListPreference mRecentClearAllPosition;
+    private CheckBoxPreference mRecentClearAll;
     private CheckBoxPreference mDualPanel;
     private CheckBoxPreference mReverseDefaultAppPicker;
 
@@ -45,6 +53,17 @@ public class SystemSettings extends SettingsPreferenceFragment implements
         mReverseDefaultAppPicker = (CheckBoxPreference) findPreference(KEY_REVERSE_DEFAULT_APP_PICKER);
         mReverseDefaultAppPicker.setChecked(Settings.System.getInt(getContentResolver(),
                     Settings.System.REVERSE_DEFAULT_APP_PICKER, 0) != 0);
+
+        mRecentClearAll = (CheckBoxPreference) prefs.findPreference(RECENT_MENU_CLEAR_ALL);
+        mRecentClearAll.setChecked(Settings.System.getInt(getContentResolver(),
+            Settings.System.SHOW_CLEAR_RECENTS_BUTTON, 1) == 1);
+        mRecentClearAll.setOnPreferenceChangeListener(this);
+        mRecentClearAllPosition = (ListPreference) prefs.findPreference(RECENT_MENU_CLEAR_ALL_LOCATION);
+        String recentClearAllPosition = Settings.System.getString(getContentResolver(), Settings.System.CLEAR_RECENTS_BUTTON_LOCATION);
+        if (recentClearAllPosition != null) {
+             mRecentClearAllPosition.setValue(recentClearAllPosition);
+        }
+        mRecentClearAllPosition.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -53,7 +72,16 @@ public class SystemSettings extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return true;
+        if (preference == mRecentClearAll) {
+            Settings.System.putInt(getContentResolver(), Settings.System.SHOW_CLEAR_RECENTS_BUTTON,
+                    (Boolean) newValue ? 1 : 0);
+            return true;
+        } else if (preference == mRecentClearAllPosition) {
+            Settings.System.putString(getContentResolver(), Settings.System.CLEAR_RECENTS_BUTTON_LOCATION, (String) newValue);
+            return true;
+        } else {
+        return false;
+        }
     }
 
     @Override
