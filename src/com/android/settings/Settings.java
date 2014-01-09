75f6856814fe9f577ab964c6892d8a2722b8e348
Andrew <ayunker551@gmail.com>
2014-01-09 18:00:38 -0500

    clean up spacing

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1130,6 +1130,6 @@ public class Settings extends PreferenceActivity
     public static class PaymentSettingsActivity extends Settings { /* empty */ }
     public static class PrintSettingsActivity extends Settings { /* empty */ }
     public static class PrintJobSettingsActivity extends Settings { /* empty */ }
-	public static class PerformanceSettingsActivity extends Settings { /* empty */ }
+    public static class PerformanceSettingsActivity extends Settings { /* empty */ }
     public static class ASSRamBarActivity extends Settings { /* empty */ } 
 }
