6de25620932e14fd189b2e6b7a3d85fa392a1f82
TheMrcool212 <munirmohamadrani@gmail.com>
2014-02-05 20:40:56 +0800

    Revert "Add global action for screen recording. [2/2]"
    This reverts commit ff3a5c42e6a4f7b112eb2a4b756b2a9e3a658e5c.

--- src/com/android/settings/AOSPAL/ScreenRecorderSettings.java
+++ src/com/android/settings/AOSPAL/ScreenRecorderSettings.java
@@ -34,12 +34,10 @@ public class ScreenRecorderSettings extends SettingsPreferenceFragment implement
     private static final String KEY_VIDEO_SIZE = "screen_recorder_size";
     private static final String KEY_VIDEO_BITRATE = "screen_recorder_bitrate";
     private static final String KEY_RECORD_AUDIO = "screen_recorder_record_audio";
-    private static final String KEY_SCREENRECORD = "power_menu_screenrecord";
 
     private ListPreference mVideoSizePref;
     private ListPreference mVideoBitratePref;
     private CheckBoxPreference mRecordAudioPref;
-    private CheckBoxPreference mScreenrecordPref;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -64,10 +62,6 @@ public class ScreenRecorderSettings extends SettingsPreferenceFragment implement
                 Settings.System.SCREEN_RECORDER_RECORD_AUDIO, 0) == 1);
         mRecordAudioPref.setOnPreferenceChangeListener(this);
         if (!hasMicrophone()) getPreferenceScreen().removePreference(mRecordAudioPref);
-
-        mScreenrecordPref = (CheckBoxPreference) findPreference(KEY_SCREENRECORD);
-        mScreenrecordPref.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.System.POWER_MENU_SCREENRECORD_ENABLED, 0) == 1));
     }
 
     @Override
@@ -85,22 +79,6 @@ public class ScreenRecorderSettings extends SettingsPreferenceFragment implement
             return true;
         }
         return false;
-    }
-
-   @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        boolean value;
-
-        if (preference == mScreenrecordPref) {
-            value = mScreenrecordPref.isChecked();
-            Settings.System.putInt(getContentResolver(),
-                    Settings.System.POWER_MENU_SCREENRECORD_ENABLED,
-                    value ? 1 : 0);
-        } else {
-            return super.onPreferenceTreeClick(preferenceScreen, preference);
-        }
-
-        return true;
     }
 
     private void updateVideoSizePreference(String value) {
