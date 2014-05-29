22d443eb738a1c2653fc87954c68f8466ddc7861
TheMrcool212 <themrcool212.android@gmail.com>
2014-05-29 18:05:40 +0800

    Add missing close bracket

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -562,6 +562,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             // Hardware abstraction framework not installed
             return false;
         }
+    }
 
     public class PackageStatusReceiver extends BroadcastReceiver {
         @Override
